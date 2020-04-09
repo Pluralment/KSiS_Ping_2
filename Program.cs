3eb52dd2afe31f02082ddd2d014ba124754e820b
Pluralment <Foolman.ru@yandex.ru>
2020-04-09 08:23:01 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            string hostname = "8.8.8.8";
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
